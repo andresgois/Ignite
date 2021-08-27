166abcdf57980ee36cfcbb27470f713ee89fe524
André gois <andre.s.gois3@gmail.com>
2021-08-27 00:06:38 -0300

    projeto finapi - finalizado

--- Finapi/src/index.js
+++ Finapi/src/index.js
@@ -12,6 +12,32 @@ const customers = [];
  * statement []
  */
 
+// Middleware
+function verifyIfExixstsAccountCPF(request, response, next){
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  if(!customer){
+    return response.status(400).json({ error: "Customer not found!"})
+  }
+
+  request.customer = customer;
+
+  next();
+}
+
+function getBalance(statement){
+  const balance = statement.reduce( (acc, operation) => {
+    if(operation.type === 'credit'){
+      return acc + operation.amount;
+    }else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -27,11 +53,97 @@ app.post("/account", (request, response) => {
     name,
     cpf,
     id: uuid4(),
-    customers: []
+    statement: []
   });
 
   return response.status(201).send();
-})
+});
+
+// tudo que estiver abaixo receberá esse middleware
+//app.use(verifyIfExixstsAccountCPF);
+app.get("/statement", verifyIfExixstsAccountCPF,(request, response) => {  
+  const { customer } = request;
+  
+  return response.json(customer.statement)
+});
+
+app.post("/deposit", verifyIfExixstsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const {customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExixstsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const {customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount){
+    return response.status(400).json({error: "Insufficient funds!"});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExixstsAccountCPF,(request, response) => {  
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter( (statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  );
+  
+  return response.json(statement)
+});
+
+app.put("/account", verifyIfExixstsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const {customer } = request;
+
+  customer.name = name;
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExixstsAccountCPF, (request, response) => {
+  const {customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExixstsAccountCPF, (request, response) => {
+  const {customer } = request;
+
+  // splice
+  customers.slice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExixstsAccountCPF, (request, response) => {
+  const {customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+});
 
 app.listen(3333, () => {
   console.log("Server rodando");
