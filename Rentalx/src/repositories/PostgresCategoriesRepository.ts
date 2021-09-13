1ac87890a34bb6bbbcba5198d7761a283bffaa38
Andre gois <andre.s.gois3@gmail.com>
2021-09-13 20:18:59 -0300

    creating an interface for the category repository

---
+++ Rentalx/src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,24 @@
+import { Category } from "../model/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
+
+
+class PostgresCategoriesRepository implements ICategoriesRepository{
+
+  findByName(name: string): Category {
+    //throw new Error("Method not implemented.");
+    console.log(name);
+    return null;
+  }
+  list(): Category[] {
+    //throw new Error("Method not implemented.");
+    return null;
+  }
+  create({ name, description} : ICreateCategoryDTO): void {
+    //throw new Error("Method not implemented.");
+    console.log(name, description);
+    return null;
+  }
+
+}
+
+export { PostgresCategoriesRepository };
---
+++ Rentalx/src/services/CreateCategoryService.ts
@@ -0,0 +1,31 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * [] Definir tipo de retorno
+ * [] ALterar o retorno de erro
+ * [] Acessar o repositorio
+ */
+
+class CreateCategoryService {
+
+  // Princípio da inversão de dependência
+  constructor(private categoriesRepository : ICategoriesRepository){}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if(categoryAlreadyExists){
+      throw new Error("Category Already Exists!");
+    }
+  
+    this.categoriesRepository.create({ name, description});
+  }
+
+}
+
+export { CreateCategoryService }
