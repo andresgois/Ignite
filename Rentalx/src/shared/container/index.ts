c68aa5d98155a283a15f1e4ac2f51b03e37f973e
Andre gois <andre.s.gois3@gmail.com>
2021-10-06 20:07:57 -0300

    creating user repository

--- Rentalx/src/shared/container/index.ts
+++ Rentalx/src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import { container } from 'tsyringe';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 
@@ -17,3 +19,8 @@ container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
 )
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+);
