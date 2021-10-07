9ba7f926d1e958f5c77f4c112eeb0d7cb7831b1b
Andre gois <andre.s.gois3@gmail.com>
2021-10-06 21:11:16 -0300

    creating user controller

--- Rentalx/src/modules/accounts/entities/User.ts
+++ Rentalx/src/modules/accounts/entities/User.ts
@@ -1,6 +1,7 @@
 import { v4 as uuidV4} from "uuid";
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 
+@Entity("users")
 class User {
 
   @PrimaryColumn()
--- Rentalx/src/routes/index.ts
+++ Rentalx/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { categoriesRoutes } from './categories.routes';
 import { specificationRoute } from './specification.route';
+import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRoute);
+router.use("/users", usersRoutes);
 
 export { router };
