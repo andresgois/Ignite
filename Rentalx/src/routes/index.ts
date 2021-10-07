6915241a6380fc377794f066bc16ecfc81598e15
Andre gois <andre.s.gois3@gmail.com>
2021-10-07 20:28:18 -0300

    authentications routes

--- Rentalx/src/routes/index.ts
+++ Rentalx/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationRoute } from './specification.route';
 import { usersRoutes } from './users.routes';
@@ -8,5 +9,6 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRoute);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
