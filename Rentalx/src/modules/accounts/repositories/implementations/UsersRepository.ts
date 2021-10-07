859f5ff7176208d054979c5931024acc5bd540d7
Andre gois <andre.s.gois3@gmail.com>
2021-10-06 21:37:22 -0300

    validation for existing email

--- Rentalx/src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ Rentalx/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -11,12 +11,18 @@ class UsersRepository implements IUsersRepository {
   constructor(){
     this.repository = getRepository(User);
   }
+  
   async create({ name, password, email, driver_license }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name, password, email, driver_license
     });
 
     await this.repository.save(user) ;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({email});
+    return user;
   }
 
 }
