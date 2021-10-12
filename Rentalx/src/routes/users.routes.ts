f107d1391480fb9a0741389102c3e6dc17c2c887
Andre gois <andre.s.gois3@gmail.com>
2021-10-11 21:22:17 -0300

    refactoring users model with new avatar field

--- Rentalx/src/routes/users.routes.ts
+++ Rentalx/src/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import { UpdateUserAvatarController } from '../modules/accounts/updateUserAvatar/UpdateUserAvatarController';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarUseCase = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle)
+
+usersRoutes.patch("/avatar", updateUserAvatarUseCase.handle);
 
 
 export { usersRoutes };
