67587644ac71427026747967e6eb5bfd854ab4ff
Andre gois <andre.s.gois3@gmail.com>
2021-10-29 19:31:18 -0300

    basis for testing

---
+++ Rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,21 @@
+
+
+
+
+/*describe("Create category", () => {
+
+  it("Expect 2 + 2 = 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result)
+  });
+
+  it("Espero que 2 + 2 não seja 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result)
+  });
+
+});*/
