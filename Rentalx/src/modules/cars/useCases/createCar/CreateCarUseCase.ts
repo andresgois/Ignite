92b8d1c52bac24a04be6a993f0a6d611da685cc6
Andre gois <andre.s.gois3@gmail.com>
2021-11-06 12:30:19 -0300

    creating use cases

---
+++ Rentalx/src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,27 @@
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository"
+import { inject, injectable } from "tsyringe"
+
+interface IRequest {
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
+
+injectable()
+class CreateCarUseCase {
+
+  constructor(
+    @inject("CarsReposirory")
+    private carsRepository: ICarsRepository
+  ){}
+
+  async execute({name, description, daily_rate, license_plate, fine_amount, brand, category_id}: IRequest): Promise<void>{
+    this.carsRepository.create({name, description, daily_rate, license_plate, fine_amount, brand, category_id});
+  }
+}
+
+export { CreateCarUseCase }
