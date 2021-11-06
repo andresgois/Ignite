7e781def3b9b4d644fed81b9e12736fc24759d06
Andre gois <andre.s.gois3@gmail.com>
2021-11-06 20:24:16 -0300

    setting the tests to verify the license and to verify that the car was created

--- Rentalx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ Rentalx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,7 @@
-import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { 
+  CarsRepositoryInMemory 
+} from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
 
@@ -12,7 +15,7 @@ describe("Create Car", () => {
   });
 
   it("should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Name Car", 
       description: "Description car", 
       daily_rate: 100, 
@@ -21,6 +24,49 @@ describe("Create Car", () => {
       brand: "Brand", 
       category_id: "category"
     });
+
+    expect(car).toHaveProperty("id");
   });
 
-})
+  it("should not be able to create a car with exists licence plate", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Name Car", 
+        description: "Description car", 
+        daily_rate: 100, 
+        license_plate: "ABC-1234", 
+        fine_amount: 60, 
+        brand: "Brand", 
+        category_id: "category"
+      });
+
+      await createCarUseCase.execute({
+        name: "Name Car2", 
+        description: "Description car2", 
+        daily_rate: 100, 
+        license_plate: "ABC-1234", 
+        fine_amount: 60, 
+        brand: "Brand2", 
+        category_id: "category2"
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to create a car with available by default", async () => {
+    
+    const car = await createCarUseCase.execute({
+      name: "Name Car", 
+      description: "Description car", 
+      daily_rate: 100, 
+      license_plate: "ABC-1234", 
+      fine_amount: 60, 
+      brand: "Brand", 
+      category_id: "category"
+    });
+    //console.log(car);
+
+    expect(car.available).toBe(true);
+
+  });
+
+});
