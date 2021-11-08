d160e7dd4178a83500015f89c5357aaf8a32a042
Andre gois <andre.s.gois3@gmail.com>
2021-11-08 19:50:05 -0300

    adding middleware to car specification

--- Rentalx/src/shared/infra/http/routes/specification.route.ts
+++ Rentalx/src/shared/infra/http/routes/specification.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const specificationRoute = Router();
 
@@ -8,6 +9,11 @@ const createSpecificationController = new CreateSpecificationController();
 
 // teste middleware
 specificationRoute.use(ensureAuthenticated);
-specificationRoute.post("/", createSpecificationController.handle);
+specificationRoute.post(
+  "/", 
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+  );
 
 export { specificationRoute };
