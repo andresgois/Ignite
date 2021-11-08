239e0d519f6e4563548f3387c9980de1114ad595
Andre gois <andre.s.gois3@gmail.com>
2021-11-08 11:28:02 -0300

    modified routes to receive middleware from admin user

--- Rentalx/src/shared/infra/http/routes/cars.route.ts
+++ Rentalx/src/shared/infra/http/routes/cars.route.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const carsRoute = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoute.post("/", createCarController.handle);
+carsRoute.post(
+  "/", 
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
 
 export { carsRoute };
