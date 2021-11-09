0e1cce9bb27c927f706cda7d44027c0985a9066e
Andre gois <andre.s.gois3@gmail.com>
2021-11-09 09:58:51 -0300

    adding the car list to the routes

--- Rentalx/src/shared/infra/http/routes/cars.route.ts
+++ Rentalx/src/shared/infra/http/routes/cars.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ListAvailablaCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailablaCarsController";
 
 const carsRoute = Router();
 
 const createCarController = new CreateCarController();
+const listAvailablaCarsController = new ListAvailablaCarsController();
 
 carsRoute.post(
   "/", 
@@ -13,5 +15,7 @@ carsRoute.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoute.get("/available", listAvailablaCarsController.handle);
 
 export { carsRoute };
