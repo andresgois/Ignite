c8e76509d8d67c499b6b39eb0d587cf8e8c0c2c2
Andre gois <andre.s.gois3@gmail.com>
2021-11-22 20:05:36 -0300

    modifying the Cars table to accept foreign keys

--- Rentalx/src/modules/cars/infra/typeorm/entities/Cars.ts
+++ Rentalx/src/modules/cars/infra/typeorm/entities/Cars.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 import { Category } from './Category';
+import { Specification } from './Specification';
 
 @Entity("cars")
 class Car {
@@ -28,16 +29,26 @@ class Car {
   @Column()
   brand: string;
 
-  @Column()
-  category_id: string;
-
+  
   /* Uma categoria pode esta disponível para um ou vários carros
-    muitos carros para uma categoria
+  muitos carros para uma categoria
   */
   @ManyToOne( () => Category)
   @JoinColumn({ name: 'category_id'})
   category: Category;
+  
+  @Column()
+  category_id: string;
 
+  @ManyToMany( () => Specification)
+  @JoinTable({
+    name: "specifications_cars",
+    joinColumns: [{name: "cars_id"}],
+    inverseJoinColumns: [{ name: "specification_id"}],
+  })
+  
+  specification: Specification[];
+ 
   @CreateDateColumn()
   created_at: Date;
 
