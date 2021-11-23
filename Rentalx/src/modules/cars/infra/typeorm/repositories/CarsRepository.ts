5b44a777f4b9c9d73531ad5ab06c208a6d4d8faf
Andre gois <andre.s.gois3@gmail.com>
2021-11-22 21:32:16 -0300

    putting the specification attribute and adding the find by id method

--- Rentalx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ Rentalx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -12,10 +12,10 @@ class CarsRepository implements ICarsRepository {
   }
   
   async create({
-    name, description, daily_rate, license_plate, fine_amount, brand, category_id
+    name, description, daily_rate, license_plate, fine_amount, brand, category_id, specifications
   }: ICreateCarDTO): Promise<Car> {
     const car = this.repository.create({
-      name, description, daily_rate, license_plate, fine_amount, brand, category_id
+      name, description, daily_rate, license_plate, fine_amount, brand, category_id, specifications
     });
     await this.repository.save(car);
     return car;
@@ -48,6 +48,11 @@ class CarsRepository implements ICarsRepository {
 
     const cars = await carsQuery.getMany();
     return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = await this.repository.findOne(id);
+    return car;
   }
 
 }
