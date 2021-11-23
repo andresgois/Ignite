dd55894d5a53f65750699c30c249f15723e6f37f
Andre gois <andre.s.gois3@gmail.com>
2021-11-23 20:37:56 -0300

    adding construct to entity to add id when it doesn't exist

--- Rentalx/src/modules/cars/infra/typeorm/entities/CarImage.ts
+++ Rentalx/src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -1,5 +1,5 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
-
+import { v4 as uuidV4 } from "uuid";
 
 @Entity("cars_image")
 class CarImage {
@@ -15,6 +15,12 @@ class CarImage {
 
   @CreateDateColumn()
   created_at: Date;
+
+  constructor() {
+    if(!this.id){
+      this.id = uuidV4()
+    }
+  }
 
 }
 
