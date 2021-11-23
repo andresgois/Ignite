b1aaac1e04de6cb5a7bc47d536db57b4ab971623
Andre gois <andre.s.gois3@gmail.com>
2021-11-22 21:32:49 -0300

    adding the find by ids method to the interface

--- Rentalx/src/modules/cars/repositories/ISpecificationsRepository.ts
+++ Rentalx/src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -9,7 +9,7 @@ interface ISpecificationsRepository {
 
   create({name, description}: ICreateSpecificationDTO): Promise<void>;
   findByName(name: string): Promise<Specification>;
-
+  findByids(ids: string[]): Promise<Specification[]>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
