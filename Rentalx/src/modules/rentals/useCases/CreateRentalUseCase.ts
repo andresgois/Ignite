8db87dcd9d35e2aaac95d3447cf099ecc5fe8ba7
Andre gois <andre.s.gois3@gmail.com>
2021-11-24 21:25:07 -0300

    rental use case creation

---
+++ Rentalx/src/modules/rentals/useCases/CreateRentalUseCase.ts
@@ -0,0 +1,36 @@
+import { AppError } from "@shared/errors/AppError";
+import { IRentalsRepository } from "../repositories/IRentalsRepository";
+
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+
+  constructor(
+    private rentalsRepository: IRentalsRepository
+  ){}
+
+  async execute({ car_id, user_id, expected_return_date}: IRequest): Promise<void> {
+
+    // O aluguel deve ter duração mínima de  24 horas
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
+
+    if(carUnavailable){
+      throw new AppError("Car is unavailable");
+    }
+    // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário.
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(user_id);
+
+    if(rentalOpenToUser){
+      throw new AppError("There's a rental in progress for users!");
+    }
+    // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
+  }
+
+}
+
+export { CreateRentalUseCase }
