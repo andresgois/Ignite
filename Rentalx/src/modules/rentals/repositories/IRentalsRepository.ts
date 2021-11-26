c88a576a3399c24d3c1dab5255f81fc3bace21b2
Andre gois <andre.s.gois3@gmail.com>
2021-11-26 19:15:30 -0300

    create method in the Rentals Repository

--- Rentalx/src/modules/rentals/repositories/IRentalsRepository.ts
+++ Rentalx/src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,3 +1,5 @@
+
+import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
 
 
@@ -6,6 +8,7 @@ interface IRentalsRepository {
 
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
 }
 
 export { IRentalsRepository };
