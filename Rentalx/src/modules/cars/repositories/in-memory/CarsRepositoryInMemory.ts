59da13a5b9264fc021b12a4e7948ea5b8877de79
Andre gois <andre.s.gois3@gmail.com>
2021-12-13 21:16:16 -0300

    implementing update Available method in Cars Repository In Memory

--- Rentalx/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ Rentalx/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -3,7 +3,6 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Cars";
 import { ICarsRepository } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsRepository {
-      
   cars: Car[] = [];
 
   async create({
@@ -40,6 +39,11 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
   async findById(id: string): Promise<Car> {
     return this.cars.find( car => car.id === id );
+  }
+
+  async updateAvailable(id: string, available: boolean): Promise<void> {
+    const findIndex = this.cars.findIndex( (car) => car.id === id);
+    this.cars[findIndex].available = available;
   }
 }
 
