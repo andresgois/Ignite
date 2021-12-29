a0c43e6792800327819eb59e91738b5a13f26dbf
Andre gois <andre.s.gois3@gmail.com>
2021-12-29 17:20:51 -0300

    adding adding hours to the contract

--- Rentalx/src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ Rentalx/src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -5,6 +5,7 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider{
+    
   compareInHours(start_date: Date, end_date: Date): number {
     const end_date_utc = this.convertToUTC(end_date);
     const start_date_utc = this.convertToUTC(start_date);
@@ -27,9 +28,15 @@ class DayjsDateProvider implements IDateProvider{
     return dayjs(end_date_utc).diff(start_date_utc, "days");
   
   }
+  
   addDays(days: number): Date {
     return dayjs().add(days, "days").toDate();
   }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, "hour").toDate();
+  }
+
 }
 
 
