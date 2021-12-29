fffec683efa142a7592aaf4449ca559000346c5d
Andre gois <andre.s.gois3@gmail.com>
2021-12-29 10:17:17 -0300

    adding findBy serIdAnd Refresh Token method to repository

--- Rentalx/src/modules/accounts/infra/repositories/UsersTokensRepository.ts
+++ Rentalx/src/modules/accounts/infra/repositories/UsersTokensRepository.ts
@@ -11,7 +11,7 @@ class UsersTokensRepository implements IUsersTokensRepository{
   constructor(){
     this.repository = getRepository(UserTokens);
   }
-  
+      
   async create({ expires_date, refresh_token, user_id }: ICreateUserTokenDTO): Promise<UserTokens> {
     const userTokens = await this.repository.create({
       expires_date,
@@ -23,6 +23,24 @@ class UsersTokensRepository implements IUsersTokensRepository{
     return userTokens;
   }
 
+  async findByUserId(user_id: string): Promise<UserTokens[]> {
+    const usersToken = await this.repository.find({
+      user_id,
+    });
+    return usersToken;
+  }
+
+  async findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
+    const usersToken = await this.repository.findOne({
+      user_id,
+      refresh_token
+    });
+    return usersToken;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
 
 }
 
