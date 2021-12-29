17997b1803478df97b6c82d306d5d9559b023ac6
Andre gois <andre.s.gois3@gmail.com>
2021-12-29 10:16:55 -0300

    adding findBy serIdAnd Refresh Token method to the interface

--- Rentalx/src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ Rentalx/src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -4,7 +4,16 @@ import { UserTokens } from "../infra/typeorm/entities/UserTokens";
 
 interface IUsersTokensRepository {
 
-  create({ expires_date, refresh_token, user_id }: ICreateUserTokenDTO): Promise<UserTokens>;
+  create({ 
+    expires_date, 
+    refresh_token, 
+    user_id 
+  }: ICreateUserTokenDTO): Promise<UserTokens>;
+
+  findByUserId(user_id: string): Promise<UserTokens[]>;
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens>;
+
+  deleteById(id: string): Promise<void>;
 
 }
 
