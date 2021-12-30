c92a5e96e6837badb364743553beaaf168cd5244
Andre gois <andre.s.gois3@gmail.com>
2021-12-30 12:26:45 -0300

    route to email recovery

--- Rentalx/src/shared/infra/http/routes/index.ts
+++ Rentalx/src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoute } from './cars.route';
 import { categoriesRoutes } from './categories.routes';
+import { passwordRoutes } from './password.routes';
 import { rentalRoutes } from './rental.routes';
 import { specificationRoute } from './specification.route';
 import { usersRoutes } from './users.routes';
@@ -13,6 +14,7 @@ router.use("/specifications", specificationRoute);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoute);
 router.use("/rentals", rentalRoutes);
+router.use("/password", passwordRoutes);
 
 
 router.use(authenticateRoutes);
