157d17cec2e6a404a57b5abf0c02b9a3bcab4a82
Andre gois <andre.s.gois3@gmail.com>
2022-01-04 20:57:28 -0300

    adding variables needed for reset

--- Rentalx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ Rentalx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -6,6 +6,7 @@ import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTok
 import { AppError } from "@shared/errors/AppError";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
+import { resolve } from 'path';
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -24,6 +25,15 @@ class SendForgotPasswordMailUseCase {
   async execute(email: string): Promise<void>{
     const user = await this.usersRepository.findByEmail(email);
 
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "emails",
+      "forgotPassword.hbs"
+    );
+
     if(!user){
       throw new AppError("User already exists!");
     }
@@ -37,10 +47,16 @@ class SendForgotPasswordMailUseCase {
       expires_date
     });
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`
+    };
+
     await this.mailProvider.sendMail(
       email,
       "Recuperação de senha",
-      `O link para o reset é: ${token}`
+      variables,
+      templatePath
     );
 
   }
