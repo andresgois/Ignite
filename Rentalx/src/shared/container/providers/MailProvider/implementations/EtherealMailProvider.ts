5665c813d55a1d6e63bd61e377c9d164193fc9a1
Andre gois <andre.s.gois3@gmail.com>
2022-01-04 20:57:58 -0300

    assigning template to email body

--- Rentalx/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ Rentalx/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,7 +1,8 @@
 import { injectable } from "tsyringe";
 import { IMailProvider } from "../IMailProvider";
 import nodemailer, { Transporter } from 'nodemailer';
-
+import handlebars from 'handlebars';
+import fs from 'fs';
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
@@ -27,13 +28,16 @@ class EtherealMailProvider implements IMailProvider {
     }).catch( (e) => console.error(e))
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+    const templateParse = handlebars.compile(templateFileContent);
+    const templateHtml = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: "Rentx <noreplay@rentx.com.br>",
       subject,
-      text: body,
-      html: body,
+      html: templateHtml,
     });
 
     console.log("Message sent: %s ", message.messageId);
