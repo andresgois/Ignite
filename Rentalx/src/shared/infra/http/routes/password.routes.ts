28084d3fc7baaf2cb8d3c106377285b75b136a45
Andre gois <andre.s.gois3@gmail.com>
2022-01-05 20:53:01 -0300

    route to password recovery

--- Rentalx/src/shared/infra/http/routes/password.routes.ts
+++ Rentalx/src/shared/infra/http/routes/password.routes.ts
@@ -1,14 +1,20 @@
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import { Router } from "express";
 
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post(
   "/forgot",
   sendForgotPasswordMailController.handle
+)
+passwordRoutes.post(
+  "/reset",
+  resetPasswordUserController.handle
 )
 
 export { passwordRoutes }
