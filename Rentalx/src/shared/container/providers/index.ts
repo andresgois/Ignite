ecbd3f9c0a2263e1b76c891c993cc2377a2fa9df
Andre gois <andre.s.gois3@gmail.com>
2022-02-28 18:46:55 -0300

    feat: dependency injection

--- Rentalx/src/shared/container/providers/index.ts
+++ Rentalx/src/shared/container/providers/index.ts
@@ -4,6 +4,9 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { IStorageProvider } from "./StorageProvider/IStorageProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 
 
 container.registerSingleton<IDateProvider>(
@@ -16,3 +19,19 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider()
 );
 
+/*
+container.registerInstance<IStorageProvider>(
+  "StorageProvider",
+  new LocalStorageProvider()
+);
+*/
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider
+}
+  
+container.registerSingleton<IStorageProvider>(
+  "StorageProvider",
+  diskStorage[process.env.DISK]
+);
