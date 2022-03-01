119e192860cf93ed582ea42a25ea133c4bb9b028
Andre gois <andre.s.gois3@gmail.com>
2022-03-01 20:57:17 -0300

    chore: files statics

--- Rentalx/src/shared/infra/http/app.ts
+++ Rentalx/src/shared/infra/http/app.ts
@@ -5,11 +5,12 @@ import "express-async-errors";
 import swaggerUi from 'swagger-ui-express';
 
 import "@shared/container";
-
+import upload from "@config/upload";
 import { AppError } from '@shared/errors/AppError';
 import { createConnection } from "typeorm";
-import { router } from './routes';
+
 import swaggerFile from '../../../swagger.json';
+import { router } from './routes';
 
 createConnection();
 const app = express();
@@ -17,6 +18,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
