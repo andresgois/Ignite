a9d66402d9a998fdfd239608062cac76479c452f
Andre gois <andre.s.gois3@gmail.com>
2022-03-03 20:36:40 -0300

    feat: provider for aws SES

---
+++ Rentalx/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,42 @@
+import { SES } from "aws-sdk";
+import fs from 'fs';
+import handlebars from 'handlebars';
+import nodemailer, { Transporter } from 'nodemailer';
+import { injectable } from "tsyringe";
+
+import { IMailProvider } from "../IMailProvider";
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+
+  private client: Transporter;
+
+  constructor(){
+    this.client = nodemailer.createTransport({
+      SES: new SES({
+        apiVersion: "2010-12-01",
+        region: process.env.AWS_BUCKET
+      })
+    })
+  }
+
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+    const templateParse = handlebars.compile(templateFileContent);
+    const templateHtml = templateParse(variables);
+
+    //const message = 
+    await this.client.sendMail({
+      to,
+      from: "Rentx <noreplay@rentx.com.br>",// E-mail válido que está cadastrado no SES da AWS
+      subject,
+      html: templateHtml,
+    });
+
+    //console.log("Message sent: %s ", message.messageId);
+    //console.log("Preview URL: %s ", nodemailer.getTestMessageUrl(message));
+  }
+
+}
+
+export { SESMailProvider }
