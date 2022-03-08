00aad9fe09a6fe406c38ad90301a47517f6b7934
Andre gois <andre.s.gois3@gmail.com>
2022-03-08 20:54:05 -0300

    fix: import

--- Rentalx/src/modules/cars/infra/typeorm/repositories/PostgresCategoriesRepository.ts
+++ Rentalx/src/modules/cars/infra/typeorm/repositories/PostgresCategoriesRepository.ts
@@ -1,6 +1,6 @@
 
 import { Category } from "@modules/cars/infra/typeorm/entities/Category";
-import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
+import { ICategoriesRepository, ICreateCategoryDTO } from "@modules/cars/repositories/ICategoriesRepository";
 
 
 class PostgresCategoriesRepository implements ICategoriesRepository{
