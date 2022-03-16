ad59f88a216c0b62232522007532e76de3ba82af
Andre gois <andre.s.gois3@gmail.com>
2022-03-15 21:16:29 -0300

    build: add cors

--- Rentalx/src/shared/infra/http/app.ts
+++ Rentalx/src/shared/infra/http/app.ts
@@ -1,13 +1,14 @@
-import "reflect-metadata";
-import "dotenv/config";
+import 'reflect-metadata';
+import 'dotenv/config';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
-import "express-async-errors";
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import "@shared/container";
-import upload from "@config/upload";
+import '@shared/container';
+import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppError';
-import { createConnection } from "typeorm";
+import { createConnection } from 'typeorm';
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
@@ -17,23 +18,24 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
-app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if(err instanceof AppError){
+    if (err instanceof AppError) {
       return response.status(err.statusCode).json({
         message: err.message,
       });
     }
 
     return response.status(500).json({
-      status: "error",
+      status: 'error',
       message: `Internal server error - ${err.message}`,
     });
   }
