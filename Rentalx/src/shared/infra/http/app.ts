2c0fb36f32fece398f2a8eb748f996536e3eb847
Andre gois <andre.s.gois3@gmail.com>
2022-03-17 20:58:15 -0300

    feat: santry

--- Rentalx/src/shared/infra/http/app.ts
+++ Rentalx/src/shared/infra/http/app.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import '@shared/container';
 import upload from '@config/upload';
@@ -19,6 +21,24 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -28,6 +48,20 @@ app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
+
+// app.use(
+//   Sentry.Handlers.errorHandler({
+//     shouldHandleError(error) {
+//       // Capture all 404 and 500 errors
+//       if (error.status === 429 || error.status === 500) {
+//         return true;
+//       }
+//       return false;
+//     },
+//   })
+// );
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
